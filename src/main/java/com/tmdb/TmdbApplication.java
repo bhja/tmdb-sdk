74da94d0881c0cfe1854f4a520cf527a4a87d754
Bhagawati <bh.ja.699@gmail.com>
2024-10-29 18:58:12 +0530

    updates

---
+++ src/main/java/com/tmdb/TmdbApplication.java
@@ -0,0 +1,13 @@
+package com.tmdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TmdbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TmdbApplication.class, args);
+    }
+
+}
